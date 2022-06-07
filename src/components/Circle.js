16cf7f4a63a79d215881b66460c22a3c95a2ffe8
hamdannur <hamdannurachid6@gmail.com>
2022-06-07 15:31:40 +0700

    hero

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const sizes = {
-    default: `w-5 h-5 bg-blue-600 gap-x-12`,
+    big: `w-5 h-5 bg-blue-600 gap-x-12`,
     small: `w-2.5 h-2.5 bg-gray-900 `,
 };
 
@@ -9,7 +9,7 @@ const Circle = ({ className = '', size }) => {
     return (
 
         <div className={`
-        ${sizes[size] || sizes.default}
+        ${sizes[size] || sizes.big}
         ${className}
         
         rounded-full
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,41 +5,31 @@ import Circle from '../components/Circle';
 const Hero = () => {
     return (
         <div>
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+                <div className="pb-16 xl:flex xl:items-center xl:justify-between gap-x-2">
+                    <div>
+                        <h1 className="mt-4 text-[32px] sm:mt-5 sm:text-[53px] lg:mt-6">
+                            <span className="block">Hi, Im Hamdan Nurachid </span>
+                            <span className="flex items-center gap-x-2">
+                                A Creative  <Circle size="big" /> UI UX Designer
+                            </span>
+                        </h1>
 
-            <div className="relative bg-red-200 overflow-hidden">
-                <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-                    <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
-                        <div className="lg:grid lg:grid-cols-12 lg:gap-8 ">
-                            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
-                                <h1>
-                                    <span className="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl">
-                                        <span className="block text-gray-900">Hi, I’m Hamdan Nurachid</span>
-                                        <span className="block text-indigo-600">A Creative  UI UX Designer</span>
-                                    </span>
-                                </h1>
-                                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-
-                                    <h3 className="mt-8 text-gray-900 leading-relaxed flex items-center gap-x-3">
-                                        Research <Circle size="small" /> UI UX Design <Circle size="small" />Prototyping
-                                    </h3>
-
-
-
-                                </p>
-
-                            </div>
-                            <div className="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
-
-                                <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+                        <h4 className="mt-4 text-lg sm:mt-5 sm:text-[32px] lg:mt-6 flex items-center gap-x-2">
+                            <span className="block">Research </span>
+                            <Circle size="small" />
+                            <span className="block">UI UX Design</span>
+                            <Circle size="small" />
+                            <span className="block">Prototyping</span>
+                        </h4>
+                        <p className="mt-5 text-xl text-gray-500">Includes every feature we offer plus unlimited projects and unlimited users.</p>
+                    </div>
+                    <div className="xl:w-2/5">
+                        <StaticImage src="../images/hamdan-ava.png" alt="icon" placeholder="none" className='w-full py-4 sm:py-0' />
 
 
-                                    <StaticImage src="../images/hamdan-ava.png" alt="icon" placeholder="none" className='w-full' />
+                    </div>
 
-
-                                </div>
-                            </div>
-                        </div>
-                    </main>
                 </div>
             </div>
 
