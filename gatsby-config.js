4dd6698fa8386df83061073ae2be2026769c07fc
hamdannur <hamdannurachid6@gmail.com>
2022-06-15 17:26:25 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -141,17 +141,17 @@ module.exports = {
 
     {
       resolve: `@decantyme/gatsby-plugin-modal-routing`,
-      options: {
-        // A selector to set react-modal's app root to, default is `#___gatsby`
-        // See http://reactcommunity.org/react-modal/accessibility/#app-element
-        appElement: '#___gatsby',
+      // options: {
+      //   // A selector to set react-modal's app root to, default is `#___gatsby`
+      //   // See http://reactcommunity.org/react-modal/accessibility/#app-element
+      //   appElement: '#___gatsby',
 
-        // Object of props that will be passed to the react-modal container
-        // See http://reactcommunity.org/react-modal/#usage
-        modalProps: {
-          modal: false
-        },
-      }
+      //   // Object of props that will be passed to the react-modal container
+      //   // See http://reactcommunity.org/react-modal/#usage
+      //   modalProps: {
+      //     modal: false
+      //   },
+      // }
     },
 
 
