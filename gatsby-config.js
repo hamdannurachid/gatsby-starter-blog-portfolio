310b56cabbdfd016c3278c671c173721be68a3f4
hamdannur <hamdannurachid6@gmail.com>
2022-06-15 18:27:47 +0700

    modal routing-3

--- gatsby-config.js
+++ gatsby-config.js
@@ -154,6 +154,21 @@ module.exports = {
       // }
     },
 
+    {
+      resolve: `gatsby-plugin-modal-routing-3`,
+      // options: {
+      //   // A selector to set react-modal's app root to, default is `#___gatsby`
+      //   // See http://reactcommunity.org/react-modal/accessibility/#app-element
+      //   appElement: '#___gatsby',
+
+      //   // Object of props that will be passed to the react-modal container
+      //   // See http://reactcommunity.org/react-modal/#usage
+      //   modalProps: {
+      //     modal: false
+      //   },
+      // }
+    },
+
 
   ],
 }
